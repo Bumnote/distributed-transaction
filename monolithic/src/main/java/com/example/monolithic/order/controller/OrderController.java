a962c4b715ca4142eeec3a4b4201209d74ec8f60
Yongbum Kim <dt10002@naver.com>
2025-10-03 15:53:41 +0900

    [Feat] Redis setnx 명령어를 활용한 Lock으로 유일성 보장

--- monolithic/src/main/java/com/example/monolithic/order/controller/OrderController.java
+++ monolithic/src/main/java/com/example/monolithic/order/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.example.monolithic.order.controller;
 
 import com.example.monolithic.order.application.OrderService;
+import com.example.monolithic.order.application.RedisLockService;
 import com.example.monolithic.order.application.dto.CreateOrderResult;
 import com.example.monolithic.order.controller.dto.CreateOrderRequest;
 import com.example.monolithic.order.controller.dto.CreateOrderResponse;
@@ -13,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
   private final OrderService orderService;
+  private final RedisLockService redisLockService;
 
-  public OrderController(OrderService orderService) {
+  public OrderController(OrderService orderService, RedisLockService redisLockService) {
     this.orderService = orderService;
+    this.redisLockService = redisLockService;
   }
 
   @PostMapping("/order")
@@ -28,7 +31,17 @@ public class OrderController {
   public void placeOrder(
       @RequestBody PlaceOrderRequest request
   ) throws InterruptedException {
-    orderService.placeOrder(request.toPlaceOrderCommand());
+    String key = "order:monolithic:" + request.orderId();
+    boolean acquiredLock = redisLockService.tryLock(key, request.orderId().toString());
+    if (!acquiredLock) {
+      throw new RuntimeException("락 획득에 실패했습니다.");
+    }
+
+    try {
+      orderService.placeOrder(request.toPlaceOrderCommand());
+    } finally {
+      redisLockService.releaseLock(key);
+    }
   }
 
 }
